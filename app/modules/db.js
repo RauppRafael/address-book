553da6e4b610cec9395e11f877d4f1c27ecb72c1
Rafael Raupp <raupperino@gmail.com>
2018-09-19 21:30:14 -0300

    Fixed prod DB and Unix testing

--- app/modules/db.js
+++ app/modules/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-const URI = process.env.NODE_ENV.trim() === 'test' ? process.env.TEST_DB_URI : process.env.DB_URI
+const URI = process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'test' ? process.env.TEST_DB_URI : process.env.DB_URI
 
-mongoose.connect(URI, { useNewUrlParser: true })
+mongoose.connect(URI, {useNewUrlParser: true})
 const db = mongoose.connection
 
 module.exports = db
