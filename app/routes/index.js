78ebaa48913db9b82660abfc3292b3e14caeabd2
Rafael Raupp <raupperino@gmail.com>
2018-09-20 02:42:35 -0300

    Added homepage

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* Redirect to github repo */
 router.get('/', (req, res) => {
-    res.redirect('https://github.com/RauppRafael/address-book')
+    res.send('Address book API - for documentation visit <a href="https://github.com/RauppRafael/address-book">the github repository</a>')
 })
 
 module.exports = router
