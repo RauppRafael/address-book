8758bd2f7eefb6f13bcac3c4817b96aee4527562
Rafael Raupp <raupperino@gmail.com>
2018-09-20 02:26:19 -0300

    Redirect to github on root route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-/* GET API routes. */
+/* Redirect to github repo */
 router.get('/', (req, res) => {
-    res.json({
-        routes: {
-            auth: {
-                login: {
-                    GET: 'Get login form',
-                    POST: 'Returns your access token'
-                },
-                register: {
-                    GET: 'Get registration form',
-                    POST: 'Creates a new account'
-                }
-            },
-        }
-    })
+    res.redirect('https://github.com/RauppRafael/address-book')
 })
 
 module.exports = router
